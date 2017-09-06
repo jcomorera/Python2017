9f25b49ab9313e374f451eb7b404dd7f6270ae68
jcomorera <joancomoreraa@gmail.com>
2017-09-06 16:51:08 +0200

    Create SUMAFIBONACCI.py

---
+++ MATH/SUMAFIBONACCI.py
@@ -0,0 +1,16 @@
+#Simple programa que suma los digitos de la seguencia de fibonacci.
+#Empezando con 1 y 2 hasta 40000.
+
+x=1
+y=2
+suma=x+y
+z=0
+while z < 40000:
+  suma= suma+(y+x)
+  if z%2==0:
+    x= y+x
+  else:
+    y=y+x
+  z +=1
+print(suma)
+  
